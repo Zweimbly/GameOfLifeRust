d1a89d9d9f50ab41f3a1b5cfe1df357fd080b813
Nasreen </>
2022-09-07 19:37:49 +0100

    Move the game code loop to update function

--- gameOfLifeRust/src/main.rs
+++ gameOfLifeRust/src/main.rs
@@ -4,43 +4,14 @@ Any live cell with two or three live neighbours survives.
 Any dead cell with three live neighbours becomes a live cell.
 All other live cells die in the next generation. Similarly, all other dead cells stay dead.
 
-  X__
-  ___
-  ___
-
-array:[
-[x,0,x],
-[x,0,0],
-[0,0,0],
-]
-arrayOfLiveCell = [(0,0)]
-
-Using arrays: size needs to be known at compile time
-Using vectors: size can change
-- Arrays work for mapping the entire board at the start, but not for tracking live cells
-
-Option using only coordinates of live cells:
-2D array of live cell coordinates - [[0,0], [1,0], [1,1]]
-
-Functions we need:
-
-get_number_live_neighbors: take in the coordinates of one cell and live cell array
-    - return integer
-
-get_coordinates_of_neighbors:
-
-array:[
-[x,0,x],
-[x,0,0],
-[0,0,0],
-]
-
 Current issues:
 - Graphics/visualization
 
 */
 
 use std::time::Duration;
+use std::io;
+use tui::{backend::CrosstermBackend, Terminal};
 
 //Takes vec of live cells and coordinates of one cell
 fn get_number_live_neighbors(live_cells: &Vec<[i32; 2]>, row: i32, col: i32) -> i32 {
@@ -94,39 +65,12 @@ fn print_board(live_cells: &Vec<[i32; 2]>, board_size: i32) {
     println!("");
 }
 
-fn main() {
-    let board_size = 40;
-    
-    let mut live_cells: Vec<[i32; 2]> = Vec::new();
-    // live_cells.push([0,0]);
-    // live_cells.push([1,0]);
-    // live_cells.push([1,1]);
-    // live_cells.push([2,1]);
-    // live_cells.push([2,2]);
-
-    // live_cells.push([4,3]);
-    // live_cells.push([4,4]);
-    // live_cells.push([5,4]);
-    // live_cells.push([5,5]);
-    // live_cells.push([6,4]);
-
-    //glider
-    live_cells.push([1,1]);
-    live_cells.push([3,1]);
-    live_cells.push([2,2]);
-    live_cells.push([2,3]);
-    live_cells.push([3,2]);
-
-    print_board(&live_cells, board_size);
-    
-
-    //for i in 1..8 {
-    loop {
+fn update(live_cells: &mut Vec<[i32; 2]>, board_size: i32) -> Vec<[i32; 2]> {
     let mut new_live_cells: Vec<[i32; 2]> = Vec::new();
     for row in 0..board_size {
         for column in 0..board_size {
-            let number_of_live_neighbors = get_number_live_neighbors(&live_cells, row, column);
-            let is_alive = is_alive(&live_cells, row, column);
+            let number_of_live_neighbors = get_number_live_neighbors(live_cells, row, column);
+            let is_alive = is_alive(live_cells, row, column);
             let should_live = should_live(number_of_live_neighbors, is_alive);
             if should_live {
                 let current_coords: [i32; 2] = [row,column];
@@ -140,9 +84,50 @@ fn main() {
         live_cells.push(cell);
     }
 
-    print_board(&live_cells, board_size);
+    print_board(live_cells, board_size);
 
     std::thread::sleep(Duration::new(1,0));
+
+    live_cells.to_vec()
+}
+
+fn main() {
+    let board_size = 40;
+
+    let mut live_cells: Vec<[i32; 2]> = Vec::new();
+
+    //glider
+    live_cells.push([1,1]);
+    live_cells.push([3,1]);
+    live_cells.push([2,2]);
+    live_cells.push([2,3]);
+    live_cells.push([3,2]);
+
+    print_board(&live_cells, board_size);
+
+    loop {
+    // let mut new_live_cells: Vec<[i32; 2]> = Vec::new();
+    // for row in 0..board_size {
+    //     for column in 0..board_size {
+    //         let number_of_live_neighbors = get_number_live_neighbors(&live_cells, row, column);
+    //         let is_alive = is_alive(&live_cells, row, column);
+    //         let should_live = should_live(number_of_live_neighbors, is_alive);
+    //         if should_live {
+    //             let current_coords: [i32; 2] = [row,column];
+    //             new_live_cells.push(current_coords);
+    //         }
+    //     }
+    // }
+    //
+    // live_cells.clear();
+    // for cell in new_live_cells {
+    //     live_cells.push(cell);
+    // }
+    //
+    // print_board(&live_cells, board_size);
+    //
+    // std::thread::sleep(Duration::new(1,0));
+    let live_cells = update(&mut live_cells, board_size);
 
     }
 }
